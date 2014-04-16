99464b4b74836736c56f6c489d4804d3d7aa9048
justinramos1 <jramos@resortmanagementsystem.com>
2014-04-16 18:09:03 -0400

    initial push

---
+++ urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'main.views.home')
+)
+
+urlpatterns += patterns(
+	'django.contrib.auth.views',
+
+	url(r'^login/$', 'login',
+		{'template_name': 'login.html'},
+		name='boardgames_login'),
+
+	url(r'^logout/$', 'logout',
+		{'next_page': 'boardgames_home'},
+		name='boardgames_logout'),
+)
